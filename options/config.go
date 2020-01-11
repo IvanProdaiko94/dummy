12de6d2a6414cd4dfc131a249f222dde95d72347
IvanProdaiko94 <prodaiko94@gmail.com>
2020-01-11 11:16:04 +0200

    remove redundant sqlconfig

--- options/config.go
+++ options/config.go
@@ -22,15 +22,6 @@ type Config struct {
 	KafkaConsumer   consumer.Config
 }
 
-type SQLDBConfig struct {
-	Host         string
-	Port         int
-	User         string
-	Pass         string
-	DBName       string
-	MaxOpenConns int
-}
-
 type KafkaTopic struct {
 	Hello string
 }
